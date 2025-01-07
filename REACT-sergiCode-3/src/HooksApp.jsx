e57b87ecc390caa005c8600d9bfad38fe08b9877
Juan Cruz <89037@sistemas.frc.utn.edu.ar>
2025-01-06 23:42:25 -0300

    se agrega custom hook de contador

---
+++ REACT-sergiCode-3/src/HooksApp.jsx
@@ -0,0 +1,10 @@
+import { ContadorComponent } from "./components/ContadorComponent";
+export const HooksApp = () => {
+  return (
+    <>
+      <h1>Aplicacion de Hooks</h1>
+      <hr />
+      <ContadorComponent></ContadorComponent>
+    </>
+  );
+};
