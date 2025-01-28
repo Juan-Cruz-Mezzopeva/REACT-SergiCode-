2e3b2ce67f8533069cee489741ceeb00b362a822
Juan Cruz <89037@sistemas.frc.utn.edu.ar>
2025-01-28 12:19:41 -0300

    curso terminado de REACT sin los proyectos

--- RRDOM-UseContext/src/App.jsx
+++ RRDOM-UseContext/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Routes, Route, Navigate} from "react-router-dom";
+import { NavBar } from "./routes/components/NavBar";
+import {HomeScreen} from "./routes/HomeScreen" 
+import {ContactScreen} from "./routes/ContactScreen" 
+import {AboutScreen} from "./routes/AboutScreen" 
+import {UsuarioProvider} from "./routes/context/UsuarioProvider"
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <UsuarioProvider>
+     <NavBar></NavBar>
+     <Routes>
+        <Route path='/' element={<HomeScreen></HomeScreen>} ></Route>
+        <Route path='/about' element={<AboutScreen></AboutScreen>} ></Route>
+        <Route path='/contact' element={<ContactScreen></ContactScreen>} ></Route>
+        <Route path='/*' element={<Navigate to={'/'}/>}  ></Route>
 
-export default App
+     </Routes>
+    </UsuarioProvider>
+  );
+};
